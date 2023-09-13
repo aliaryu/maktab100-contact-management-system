1a54ba7614283e02e2507c3f28c72290f0555535
aliaryu <aliaryu@yahoo.com>
2023-09-13 17:25:01 +0330

    create descriptors for contact

--- contact_manager/validators.py
+++ contact_manager/validators.py
@@ -1,2 +1,55 @@
+import re
 
-"""in this module, i should create validations for user, pass, email, etc..."""
+
+class NameDescriptor:
+    def __get__(self, instance, owner):
+        return instance._name
+    
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            instance._name = value
+        else:
+            raise ValueError(f"name '{value}' must be string.")
+
+
+class EmailDescriptor:
+    def __get__(self, instance, owner):
+        return instance._email
+    
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            if re.match(r"^\S+@\S+\.\S+$", value):
+                instance._email = value
+            else:
+                raise ValueError(f"'{value}' is not valid email.")
+        else:
+            raise ValueError(f"email '{value}' must be string.")
+
+
+class EmailDescriptor:
+    def __get__(self, instance, owner):
+        return instance._email
+    
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            if re.match(r"^\S+@\S+\.\S+$", value):
+                instance._email = value
+            else:
+                raise ValueError(f"'{value}' is not valid email.")
+        else:
+            raise ValueError(f"email '{value}' must be string.")
+
+
+class PhoneDescriptor:
+    def __get__(self, instance, owner):
+        return instance._phone
+    
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            if re.match(r"09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}", value):
+                instance._phone = value
+            else:
+                raise ValueError(f"'{value}' is not valid phone.")
+        else:
+            raise ValueError(f"phone '{value}' must be string.")
+
